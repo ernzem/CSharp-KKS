85b459ec942fb957523b92f958d79f8ec8afbfbc
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2020-06-05 21:18:27 +0300

    sollution

---
+++ uzduotys/2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Žinomi kambario matmenys –ilgis ir plotis. Abu dydžiai sveikieji skaičiai. 
+			 * Reikia apskaičiuoti, kokią pinigų sumą reiks mokėti už plyteles, skirtas kambario grindims kloti, jei plytelių vieno kvadrato kaina yra kaina litų. 
+			 * Plytelių reikia pirkti 5 proc. daugiau galimiems nuostoliams padengti.*/
+
+			Console.WriteLine("Įvesti kambario ilgį:");
+			int ilgis = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Įvesti kambario plotį:");
+			int plotis = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Įvesti plytelių kaina:");
+			double kv_kaina = Convert.ToDouble(Console.ReadLine());
+
+			double atsarga = 0.05f;
+
+			double bendra_kaina = Math.Round(ilgis * plotis * kv_kaina * (1 + atsarga), 2);
+			Console.WriteLine("Plytelių kaina: {0} Eur.", bendra_kaina);
+
+		}
+	}
+}
